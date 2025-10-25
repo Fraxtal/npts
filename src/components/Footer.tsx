b6b121a59a84bd9e29d9f2e9dc476aea45fc29c9
Fraxtal <nicholaspangts@gmail.com>
2025-10-26 02:22:32 +0800

    Footer Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,21 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <p>H</p>
+    <footer className="w-full pt-12">
+      <div className="mx-auto flex max-w-3xl flex-col items-center justify-center px-8 pb-32 sm:flex-row-reverse sm:justify-between">
+        <section className="mt-8 text-center sm:mt-0 sm:text-left">
+          <p className="text-xs text-muted-foreground">
+            &copy; {new Date().getFullYear()}{" "}
+            <Link className="link" href="/">
+              Home
+            </Link>{" "}
+            |{" "}
+            <Link className="link font-bold" href="/privacy">
+              Privacy Policy
+            </Link>
+          </p>
+        </section>
+      </div>
+    </footer>
   );
 }
