4182aa63cce9bb79afa9bb8681630b2f76825c75
Fraxtal <nicholaspangts@gmail.com>
2025-10-27 09:31:42 +0800

    Added Schemas and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import RoundedImage from "@/components/RoundImage";
+import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import {
   ArrowDown,
@@ -11,7 +12,7 @@ import path from "path";
 
 const BIRTH_YEAR = 2005;
 
-export default function Home() {
+export default async function Home() {
   return (
     <>
       <article className="mt-8 flex flex-col gap-16 pb-16">
@@ -31,16 +32,26 @@ export default function Home() {
 
             <div className="mt-6 flex items-center gap-1">
               <p className="text-balance text-sm font-semibold sm:text-base">
-                For any enquiries, do contact me with the following links
+                My links
               </p>
               <ArrowDownRight className="hidden size-5 animate-bounce sm:block" />
               <ArrowDown className="block size-5 animate-bounce sm:hidden" />
             </div>
+            <div className="mt-5 flex items-center gap-4">
+              <Link href="/resume.pdf" target="_blank">
+                <Button variant="outline">
+                  <span className="font-semibold">Resume</span>
+                  <FileDown className="ml-2 size-5" />
+                </Button>
+              </Link>
+              <Socials />
+            </div>
+            
           </div>
           <RoundedImage className="md:mr-8" />
-          
-        </section>
 
+        </section>
+        
 
       </article>
     </>
